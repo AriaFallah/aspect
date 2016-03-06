b03d8d53c95a1727255eb645eccb53a68eec3e19
Robert Adkins <robertadkins00@gmail.com>
2016-03-05 22:10:14 -0500

    added more http stuff that will go away soon

--- src/facialysis.js
+++ src/facialysis.js
@@ -1,6 +1,22 @@
-const SERVER  = 'https://api.kairos.com/media/'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createFetch, base, accept, parseJSON } from 'http-client'
+
+const SERVER  = 'https://api.kairos.com/'
 const APP_ID  = 'c7a9d520'
 const APP_KEY = 'a795ec792f80a82aeefc3290e2829372'
+
+const fetch = createFetch(
+  base(SERVER),
+  header('app_id' , APP_ID)
+  header('app_key', APP_KEY)
+  accept('application/json'),
+  parseJSON()
+)
+
+fetch('/media').then((response) => {
+  console.log(response.jsonData)
+})
 
 function dank_face(vid) {
   xhttp.open('POST', SERVER, false)
